24e7a8bfe6b449c4a6155240aeaf9b1af456b75f
Karen G <kgraves0701@gmail.com>
2017-10-21 04:51:58 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,7 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+/*const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 var index = 0
+
 
 function onKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which);
@@ -17,3 +18,24 @@ function onKeyDownHandler(e) {
       index = 0;
     }
   }
+  */
+
+function init(){
+}
+
+init();
+
+  if (window.addEventListener) {
+    var index = [],
+        code = "38,38,40,40,37,39,37,39,66,65";
+    window.addEventListener("keydown", function(e){
+        index.push(e.keyCode);
+
+        //if (index.toString().indexOf(code) >= 0) {
+        if (index === code.length){
+             alert('Hurray!');
+
+            index = [];
+        };
+    }, true);
+};
